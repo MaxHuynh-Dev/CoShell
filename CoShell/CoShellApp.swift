666c4dc6d9b6d06f1ed2471d71a5dee659977827
max <max>
2026-01-06 11:50:36 +0700

    Initial Commit

---
+++ CoShell/CoShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoShellApp.swift
+//  CoShell
+//
+//  Created by Đạt Huỳnh on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CoShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
