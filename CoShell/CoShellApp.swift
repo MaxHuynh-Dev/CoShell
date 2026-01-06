c0f0d997a25f28ebbc71a2ed9e7f71e2eb3aadc2
max <max>
2026-01-06 16:23:41 +0700

    First commit init app

--- CoShell/CoShellApp.swift
+++ CoShell/CoShellApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct CoShellApp: App {
+    // Thêm init này để làm chậm app lại 2 giây
+    init() {
+        Thread.sleep(forTimeInterval: 2.0)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- CoShell/ContentView.swift
+++ CoShell/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var age: Int = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.secondarySystemBackground)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 5) {
+                HStack(spacing: 15) {
+                    Image("avatar")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.blue)
+                        .padding(.bottom, 10)
+                     
+                    Text("Đạt")
+                        .font(.title)
+                        .bold()
+                        .padding(.bottom, 10)
+                }
+                
+                Text("Front end developer")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Button(action: {
+                    print("Đã follow")
+              
+                    print("age: \(age)")
+                    age += 1
+                }) {
+                    Text("Theo dõi")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 20)
+                Text("Ho Chi Minh City, VN")
+                    .padding(.top, 20)
+            }
+            .padding(30)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
+            .padding()
         }
-        .padding()
+        
     }
 }
 
